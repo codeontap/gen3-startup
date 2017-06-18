aee18f6afba49b8fa6bfb669222fe77b17e96661
Michael Leditschke <mike@ammd.com.au>
2017-06-18 13:09:56 +1000

    Switch to cloud watch logging as the default

--- bootstrap/centos/ecs.sh
+++ bootstrap/centos/ecs.sh
@@ -7,31 +7,41 @@ ACCOUNT=$(/etc/codeontap/facts.sh | grep cot:account= | cut -d '=' -f 2)
 
 # Check if a template for the ECS config has been provided
 aws --region ${REGION} s3 cp s3://${CREDENTIALS}/${ACCOUNT}/alm/docker/ecs.config /etc/ecs/ecs.config
-if [[ ! -f /etc/ecs/ecs.config ]]; then
-cat > /etc/ecs/ecs.config << EOF
-ECS_AVAILABLE_LOGGING_DRIVERS=["json-file","syslog","journald","gelf","fluentd"]
-ECS_LOGLEVEL=warn
-EOF
+
+# Capture the current configuration
+if [[ -f /etc/ecs/ecs.config ]]; then
+    . /etc/ecs/ecs.config
+else
+    touch /etc/ecs/ecs.config
+    # Defaults
+    [[ -z "${ECS_AVAILABLE_LOGGING_DRIVERS}" ]] && ECS_AVAILABLE_LOGGING_DRIVERS="[\"json-file\",\"syslog\",\"journald\",\"gelf\",\"fluentd\",\"awslogs\"]"
+    [[ -z "${ECS_LOGLEVEL}" ]] && ECS_LOGLEVEL=warn
+    [[ -z "${ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION}" ]] && ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=10m
 fi
 
-# Add the cluster
-echo ECS_CLUSTER=$ECS_CLUSTER >> /etc/ecs/ecs.config
+# Update the ECS agent configuration
+SETTINGS=("ECS_CLUSTER" "ECS_LOGLEVEL" "ECS_AVAILABLE_LOGGING_DRIVERS" "ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION" )
+for SETTING in "${SETTINGS[@]}" ; do
+    if [[ -n "${!SETTING}" ]]; then
+        if grep "${SETTING}" /etc/ecs/ecs.config  ; then
+            sed -i "s,^\(${SETTING}=\).*,\1${!SETTING},g" /etc/ecs/ecs.config
+        else
+            echo "${SETTING}=${!SETTING}" >> /etc/ecs/ecs.config
+        fi
+    fi
+done
 
-#
-# Add log driver to docker startup options if provided
-if [[ "${ECS_LOGLEVEL}" != "" ]]; then
-	echo ECS_LOGLEVEL=$ECS_LOGLEVEL >> /etc/ecs/ecs.config
-fi
-
-#
-# Add log driver to docker startup options if provided
-if [[ "${ECS_LOG_DRIVER}" != "" ]]; then
+# Add default log driver to docker startup options if provided
+# If its cloud watch logging, ignore
+if [[ (-n "${ECS_LOG_DRIVER}") && ("${ECS_LOG_DRIVER}" != "awslogs") ]]; then
 	. /etc/sysconfig/docker
-	if [[ "$(echo $OPTIONS | grep -- --log-driver )" == "" ]]; then
-		echo OPTIONS="\"${OPTIONS} --log-driver=${ECS_LOG_DRIVER}\"" >> /etc/sysconfig/docker
+	if [[ -n "${OPTIONS}" ]]; then
+            sed -i "s,^\(OPTIONS=\).*,\1\"${OPTIONS} --log-driver=${ECS_LOG_DRIVER}\",g" /etc/ecs/ecs.config
+	else
+		echo "OPTIONS=\"--log-driver=${ECS_LOG_DRIVER}\"" >> /etc/sysconfig/docker
 	fi
 fi
-#
+
 # Restart docker to ensure it picks up any EBS volume mounts and updated configuration settings
 # - see https://github.com/aws/amazon-ecs-agent/issues/62
 /sbin/service docker restart 
