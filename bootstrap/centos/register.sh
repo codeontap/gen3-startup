f70dd16bc253803f451106fe5e6c093b1fa5333d
roleyfoley <roleyfoley@gmail.com>
2018-04-25 12:12:48 +1000

    Remove use of curl for registration

--- bootstrap/centos/register.sh
+++ bootstrap/centos/register.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -ex
 # Assumes load balancer is passed as an environment variable
 exec > >(tee /var/log/codeontap/register.log|logger -t codeontap-register -s 2>/dev/console) 2>&1
-INSTANCE=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
-aws elb register-instances-with-load-balancer --load-balancer-name $LOAD_BALANCER --instances $INSTANCE
+INSTANCE=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+AVAILABILITY_ZONE="$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)"
+REGION=${AVAILABILITY_ZONE::-1}
+aws --region "${REGION}" elb register-instances-with-load-balancer --load-balancer-name $LOAD_BALANCER --instances $INSTANCE
 #
 
 
